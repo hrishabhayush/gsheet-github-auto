cff93b98fad72d0dc122767fbecc1953f04fb701
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-07-01 14:53:03 -0400

    used regex to extract useful information

--- sync.py
+++ sync.py
@@ -3,6 +3,9 @@ from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
 import os
 import pandas as pd
+import requests
+import hashlib
+import re
 
 load_dotenv()
 
@@ -12,10 +15,10 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-'''
-Returns the first worksheet and the dataframe with all the records
-'''
 def open_file(creds, spreadsheet_url):
+    '''
+    Returns the first worksheet and the dataframe with all the records
+    '''
     try:    
         client = gspread.authorize(creds)    
         # Store the spreadsheet object
@@ -38,26 +41,82 @@ def open_file(creds, spreadsheet_url):
     except Exception as e:
         print(f"Unexpected error: {e}")
 
-'''
-This function will have the automation script
-'''
 def write_file(creds, spreadsheet_url):
+    '''
+    This function will have the automation script from the github to write to the google sheets
+    '''
     ws, _ = open_file(creds=creds, spreadsheet_url=spreadsheet_url)
-    ws.update_acell('B1', 'Bingo!')
-    return val
+    ws.update_acell('B1', 'Bingo!')    
 
+def get_readme(url):
+    ''' 
+    Fetches the raw README.md of the github repository
+    '''
+    readme = requests.get(url).text
+    return readme
+
+
+def extract_information(url):
+    '''
+    Extract the internship Markdown table from the README.md
+    '''
+    readme_text = get_readme(url)
+    pattern = r'\| Company \| Role \| Location \| Application/Link \| Date Posted \|\n\|[-| ]+\|\n((?:\|.*\|\n?)*)'
+
+    match = re.search(pattern, readme_text)
+
+    if match:
+        table_body = match.group(1)
+        full_table = (
+                    "| Company | Role | Location | Application/Link | Date Posted |\n"
+        "| ------- | ---- | -------- | ---------------- | ----------- |\n"
+        + table_body
+        )
+        print(full_table)
+    else:
+        raise Exception("Internship table not found.")
+
+
+def detect_changes(url):
+    '''
+    Detect whether the raw README.md has changed or not 
+    '''
+    readme = get_readme(url)
+    readme_hash = hashlib.sha256(readme.encode()).hexdigest()
+    hash_path = "last_hash.txt"
+    last_hash = ""
+
+    if os.path.exists(hash_path):
+        with open(hash_path, "r") as f:
+            last_hash = f.read().strip()
+    
+    if readme_hash == last_hash:
+        print("No changes detected")
+        exit()
+
+    # Save the new hash 
+    with open(hash_path, "w") as f:
+        f.write(readme_hash)
     
 def main():
+    '''
+    The main function
+    '''
     print("STARTING GOOGLE SHEETS AUTOMATION")
     creds = Credentials.from_service_account_file("credentials.json", scopes=scope)
 
     spreadsheet_url = os.getenv('GOOGLE_SPREADSHEET_LINK')
     if not spreadsheet_url:
         raise ValueError("GOOGLE_SPREADSHEET_LINK environment variable is not set")
+    
+    github_url = "https://raw.githubusercontent.com/vanshb03/Summer2026-Internships/main/README.md"
+
     ws = open_file(creds, spreadsheet_url)
-    value = write_file(creds, spreadsheet_url)
-    values_list = ws.row_values(1)
-    print(values_list)
-    print(value)
+    # value = write_file(creds, spreadsheet_url)
+    # values_list = ws.row_values(1)
+    # readme = get_readme(github_url)
+    extract_information(github_url)
+    # print(values_list)
+    # print(value)
 
 main()
